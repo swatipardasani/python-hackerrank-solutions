2795be2ae4bb0f9273d78e8e41045a5119711846
Swati Pardasani <31573162+swatipardasani@users.noreply.github.com>
2021-12-02 00:20:58 +0530

    Create Loops.py

---
+++ Loops.py
@@ -0,0 +1,7 @@
+# Loops -- https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+import math
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n) :
+        print(pow(i,2))
